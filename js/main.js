72dde52b561413236b12401e5e6ef12d3c6fe7b9
unknown <lavellthedev+github@gmail.com>
2018-08-30 12:41:20 +0900

    adding javascript

--- js/main.js
+++ js/main.js
@@ -19,4 +19,10 @@
 ///--------------------------------------------------------------------------------///
 ///~~~~~~~~~~~~~~~~~ACTUAL---CODE~~~~~~~~~~~~~~~~~~///
 ///-------------------------------------------------------------------------------///
-alert("Yawk Yawk")
+const nav = document.querySelector('#mainNav');
+const topofNav = nav.offsetTop;
+function fixNav(){
+  console.log(topofNav, window.scrollY);
+}
+
+window.addEventListener('scroll',fixNav);
