a87efa1f0a4cd98bca4f4adb4d2cb07a5e0cf4bc
unknown <lavellthedev+github@gmail.com>
2018-09-11 22:43:49 +0900

    faq details

--- js/main.js
+++ js/main.js
@@ -70,23 +70,23 @@ $('a[href*="#"]')
   });
 
 //Start of FAQ CODE()
-function faqControl(){
-  $('.a').hide();
-  $('.q').on("click", function(){
-    var answerLi = $(this).next();
-    if($(this).attr("data-icon") === "+"){
-      $(this).attr("data-icon", "-");
-      answerLi.slideDown();
-    }else{
-      $(this).attr("data-icon", "+")
-      answerLi.slideUp();
-    };
-  });
-}
-
-$(document).ready(function(){
-  faqControl();
-});
+// function faqControl(){
+//   $('.a').hide();
+//   $('.q').on("click", function(){
+//     var answerLi = $(this).next();
+//     if($(this).attr("data-icon") === "+"){
+//       $(this).attr("data-icon", "-");
+//       answerLi.slideDown();
+//     }else{
+//       $(this).attr("data-icon", "+")
+//       answerLi.slideUp();
+//     };
+//   });
+// }
+//
+// $(document).ready(function(){
+//   faqControl();
+// });
 //--------------------------------------------------------------
 function faqControl(){
 $('.a').hide();
