681f9b6b33ac78205f69154970c869a01a7e6d43
Lavell Wesley <lavellthedev+github@gmail.com>
2018-11-28 20:16:11 -0500

    added redline API

--- js/custom.js
+++ js/custom.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
 
     var userFeed = new Instafeed({
         get: 'user',
-        userId: '4781521505',
+        userId: '5417867077',
         limit: 10,
         resolution: 'standard_resolution',
-        accessToken: '4781521505.6756e1c.94dc01eff566411f9b068933663d9704',
+        accessToken: '5417867077.4e5cb54.cd37a8f572c349b49c794171d487fac9',
         sortBy: 'most-recent',
         template: '<div class="col-lg-3 instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
     });
